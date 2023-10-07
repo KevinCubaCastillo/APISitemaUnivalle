f1ed22b40ac1dd089766642cb586ad0abeaff93f
Kevin Cuba Castillo <cck2023657@est.univalle.edu>
2023-10-06 21:08:05 -0400

    Publicaciones

---
+++ APISitemaUnivalle/Models/Request/Publicacion/Publicacion_add_Request.cs
@@ -0,0 +1,13 @@
+﻿namespace APISitemaUnivalle.Models.Request.Publicacion
+{
+    public class Publicacion_add_Request
+    {
+       // public int Id { get; set; }
+        public string Archivo { get; set; } = null!;
+        public string? Descripcion1 { get; set; }
+        public int ServiciosId { get; set; }
+        public string Titulo { get; set; } = null!;
+        public string? Descripcion2 { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ APISitemaUnivalle/Models/Request/Servicios/servicio_add_request.cs
@@ -0,0 +1,9 @@
+﻿namespace APISitemaUnivalle.Models.Request.Servicios
+{
+    public class servicio_add_request
+    {
+        public string Nombre { get; set; } = null!;
+        public int ModuloId { get; set; }
+        public string? ImagenUrl { get; set; }
+    }
+}
