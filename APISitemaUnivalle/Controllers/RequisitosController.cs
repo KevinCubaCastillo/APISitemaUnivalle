8a0240eb5c5c0e5dc89b01c1b3702747cdcc2c70
DafneVargas532002 <vmd2022928@est.univalle.edu>
2023-10-10 14:06:57 -0400

    Endpoint "getRequisitosbyServicioId/{id}"

--- APISitemaUnivalle/Controllers/RequisitosController.cs
+++ APISitemaUnivalle/Controllers/RequisitosController.cs
@@ -293,5 +293,44 @@ namespace APISitemaUnivalle.Controllers
             }
             return Ok(oResponse);
         }
+
+
+
+
+        [HttpGet("getRequisitosbyServicioId/{id}")]
+        public IActionResult getRequisitosbyServicioId(int id)
+        {
+            Response oResponse = new Response();
+            try
+            {
+
+                var datos = _context.Requisitos.Where(r => r.Estado == true && r.ServiciosId == id).Select(i => new
+                {
+                    Identificador = i.Id,
+                    descripcion = i.Descripcion,
+                    servicio = i.Servicios.Nombre,
+                    pasosRequisito = i.PasosRequisitos.Select(d => new
+                    {
+                        Identificador = d.Id,
+                        d.Nombre,
+                        Requisito = d.Requisitos.Descripcion,
+                    })
+                });
+                if (datos.Count() == 0)
+                {
+                    oResponse.message = "No se encontraron datos";
+                    return NotFound(oResponse);
+                }
+                oResponse.data = datos;
+                oResponse.success = 1;
+                oResponse.message = "Solicitud realizado con exito";
+            }
+            catch (Exception ex)
+            {
+                oResponse.message = ex.Message;
+                return BadRequest(oResponse);
+            }
+            return Ok(oResponse);
+        }
     }
 }
